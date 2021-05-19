567c1af19d5db8085348ba7e8e6eeaa255922ed2
Eric Ross Bernstein <ericrossbernstein@gmail.com>
2021-05-19 14:53:36 -0700

    Updated with async/await, aggregates, and joins

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -14,6 +14,14 @@ async function listOutOfStockCount(req, res, next) {
   res.json({ data: await productsService.listOutOfStockCount() });
 }
 
+async function listPriceSummary(req, res, next) {
+  res.json({ data: await productsService.listPriceSummary() });
+}
+
+async function listTotalWeightByProduct(req, res) {
+  res.json({ data: await productsService.listTotalWeightByProduct() });
+}
+
 function read(req, res) {
   const { product: data } = res.locals;
   res.json({ data });
@@ -28,4 +36,6 @@ module.exports = {
   read: [asyncErrorBoundary(productExists), read],
   list: asyncErrorBoundary(list),
   listOutOfStockCount: asyncErrorBoundary(listOutOfStockCount),
+  listPriceSummary: asyncErrorBoundary(listPriceSummary),
+  listTotalWeightByProduct: asyncErrorBoundary(listTotalWeightByProduct),
 };
